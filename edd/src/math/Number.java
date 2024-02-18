01f869dcce08f12c88e4a6c46e5e8183fc9ab451
Leonardo Gallo <lgallo@ciencias.unam.mx>
2024-02-17 23:08:51 -0600

    add Number

---
+++ edd/src/math/Number.java
@@ -0,0 +1,21 @@
+package math;
+
+import java.lang.Comparable;
+
+/**
+ * COMMENT...
+ * @author 
+ */
+public abstract class Number {
+
+    /**
+     * COMMENT...
+     */
+    public abstract int getIntegerPart();
+
+    /**
+     * COMMENT...
+     */
+    public abstract double getDecimalPart();
+
+}
