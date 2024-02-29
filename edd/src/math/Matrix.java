fc11fe10189b7a729430dfc8fb78e032dd92dea6
Leonardo Gallo <lgallo@ciencias.unam.mx>
2024-02-29 17:57:22 -0600

    add Matrix

---
+++ edd/src/math/Matrix.java
@@ -0,0 +1,27 @@
+package math;
+
+/**
+ * Comment...
+ *
+ */
+public abstract class Matrix<T> {
+
+    /**
+     * Comment...
+     *
+     */
+    public int getNColumns();
+
+    /**
+     * Comment...
+     *
+     */
+    public int getNRows();
+
+    /**
+     * Comment...
+     *
+     */
+    public T getElement(int i, int j);
+
+}
