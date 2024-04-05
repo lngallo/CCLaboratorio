f85fc244ce7415e569f78dd0db5d48d9cd87b143
Leonardo Gallo <lgallo@ciencias.unam.mx>
2024-04-04 18:57:54 -0600

    add Towers og Hanoi

---
+++ edd/src/games/TowersOfHanoi.java
@@ -0,0 +1,41 @@
+package games;
+
+import structures.Stack;
+
+/**
+ * COMÉNTAME...
+ * @author 
+ */
+public class TowersOfHanoi {
+    
+    /** Variables y objetos de clase */
+    public static int n;
+    public static Stack<Integer>[] torres = new Stack[3];
+    
+    /**
+     * Función para colocar los discos en la primer torre,
+     * pinta el estado de las torres e inicia el primer movimiento.
+     * @param n
+     */
+    public static void init(int n) {
+	// aquí va tu implementación...
+    }
+    
+    /**
+     * Función recursiva para mover los discos.
+     * @param n
+     * @param origen
+     * @param auxiliar
+     * @param destino 
+     */
+    public static void move(int n, int origin, int aux, int destiny) {
+        //aquí va tu implementación...
+    }
+    
+    /**
+     * Función que muestra en consola el estado actual de las torres.
+     */
+    public static void show() {
+        //aquí va tu implementación...
+    }
+}
