8ba645abe90688c416b3221651d5043124414928
Leonardo Gallo <lgallo@ciencias.unam.mx>
2024-04-18 17:51:13 -0600

    Se sube clase de uso de la biblioteca

---
+++ edd/src/ui/UseLibrary.java
@@ -0,0 +1,35 @@
+package ui;
+
+import apps.Library;
+import java.util.Scanner;
+
+/**
+ *
+ *
+ */
+public class UseLibrary {
+
+
+    /**
+     *
+     */
+    private static void getMenu() {
+	System.out.println("Bienvenida...."
+			   + "Seleccione la opción..."
+			   + "\n[1] Llena acervo...");
+
+    }
+
+    public static void main(String[] args) {
+
+	Library<String> alejandria = new Library<>();
+
+	Scanner sc = new Scanner(System.in);
+
+	do {
+	    getMenu();
+
+	} while(false);
+
+    }
+}
