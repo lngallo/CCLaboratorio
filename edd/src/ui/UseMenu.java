084c95dfa418113857b3aa20ac8314747c595f1d
Leonardo Gallo <lgallo@ciencias.unam.mx>
2024-05-31 13:03:36 -0600

    Sub menús

--- edd/src/ui/UseMenu.java
+++ edd/src/ui/UseMenu.java
@@ -22,7 +22,8 @@ public class UseMenu {
 			   + "\n1. Agregar"
 			   + "\n2. Actualizar"
 			   + "\n3. Eliminar"
-			   + "\n4. Buscar");
+			   + "\n4. Buscar"
+			   + "\n0. Salir");
     }
 
     /**
@@ -38,10 +39,44 @@ public class UseMenu {
 
 	System.out.print("Opción: ");
 	opcion = sc.nextInt();
-	if (opcion != 5) {
-	    flag = true;
-	} else {
+
+	switch (opcion) {
+	case 1:
+	    do {
+		cleanScreen();
+		System.out.println("Selecciones una de las siguientes opciones:"
+				   + "\n1. Ejecutar"
+				   + "\n2. Regresar al menú principal."
+				   + "\n0. Salir");
+		opcion = sc.nextInt();
+		switch (opcion) {
+		case 1:
+		    flag = false;
+		    break;
+		case 2:
+		    flag = false;
+		    break;
+		case 0:
+		    flag = false;
+		    break;
+		default:
+		    System.out.println("Opción no valida, intenta de nuevo.");
+		    flag = true;
+		    break;
+		}
+	    } while (flag);
+	case 2:
+	    break;
+	default:
+	    System.out.println("La opción no es válida.");	    
+	}
+	
+	if (opcion == 0) {
+	    System.out.println("Vuelve pronto....");
+	    // que se viusalice la pantalla 3 segundo.
 	    flag = false;
+	} else {
+	    flag = true;
 	}
 	
 	}while (flag);
